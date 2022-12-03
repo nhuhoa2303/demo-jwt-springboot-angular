698cdb82e366a3968f72d28ee1e998546ca9ef26
unknown <nguyennhuhoa2303@gmail.com>
2022-12-03 16:39:31 +0700

    up

---
+++ demo-jwt-fe/src/app/board-staff/board-staff.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board-staff',
+  templateUrl: './board-staff.component.html',
+  styleUrls: ['./board-staff.component.css']
+})
+export class BoardStaffComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ demo-jwt/src/main/java/com/example/demo/payload/request/LoginRequest.java
@@ -0,0 +1,38 @@
+package com.example.demo.payload.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+public class LoginRequest {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+
+    public LoginRequest() {
+    }
+
+    public LoginRequest( String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
